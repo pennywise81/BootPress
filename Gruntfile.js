9fae029134af2f44feffe5fcb045fc3292b4e179
pennywise81 <pennywise81@users.noreply.github.com>
2015-08-14 14:11:29 +0200

    some more tidying, added a watcher for scss

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,10 +26,18 @@ module.exports = function(grunt) {
           'style.css': 'scss/style.scss'
         }
       }
+    },
+
+    watch: {
+      css: {
+        files: ['**/*.scss'],
+        tasks: ['sass']
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 };
